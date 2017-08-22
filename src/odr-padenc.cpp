46464f05c951b033b4c4d6449aef3c05c67d0b38
Stefan Pöschel <github@basicmaster.de>
2017-08-22 20:44:25 +0200

    Move includes/static vars into new main header

--- src/odr-padenc.cpp
+++ src/odr-padenc.cpp
@@ -27,33 +27,13 @@
     \author Stefan Pöschel <odr@basicmaster.de>
 */
 
-#include "common.h"
-
-#include <stdlib.h>
-#include <signal.h>
-#include <string>
-#include <thread>
-#include <vector>
-#include <sys/types.h>
-#include <fcntl.h>
-#include <getopt.h>
-
-#include "pad_common.h"
-#include "dls.h"
-#include "sls.h"
-
-
-static const int SLEEPDELAY_DEFAULT = 10; // seconds
-static const int DLS_REPETITION_WHILE_SLS = 50;
-
-static bool do_exit = false;
+#include "odr-padenc.h"
 
 
 static void break_handler(int) {
     fprintf(stderr, "...ODR-PadEnc exits...\n");
     do_exit = true;
 }
-
 
 static void header() {
     fprintf(stderr, "ODR-PadEnc %s - DAB PAD encoder for MOT Slideshow and DLS\n\n"
---
+++ src/odr-padenc.h
@@ -0,0 +1,49 @@
+/*
+    Copyright (C) 2014 CSP Innovazione nelle ICT s.c.a r.l. (http://rd.csp.it/)
+
+    Copyright (C) 2014, 2015 Matthias P. Braendli (http://opendigitalradio.org)
+
+    Copyright (C) 2015, 2016, 2017 Stefan Pöschel (http://opendigitalradio.org)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/*!
+    \file odr-padenc.h
+    \brief Generate PAD data for MOT Slideshow and DLS
+
+    \author Sergio Sagliocco <sergio.sagliocco@csp.it>
+    \author Matthias P. Braendli <matthias@mpb.li>
+    \author Stefan Pöschel <odr@basicmaster.de>
+*/
+
+#include "common.h"
+
+#include <stdlib.h>
+#include <signal.h>
+#include <string>
+#include <thread>
+#include <vector>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <getopt.h>
+
+#include "pad_common.h"
+#include "dls.h"
+#include "sls.h"
+
+
+static const int SLEEPDELAY_DEFAULT = 10; // seconds
+static const int DLS_REPETITION_WHILE_SLS = 50;
+
+static bool do_exit = false;
